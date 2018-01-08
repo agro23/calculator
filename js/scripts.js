21cddc41bed4471056d8c8c1c1d268960f8a0b1d
Andy Grossberg <andy.grossberg@gmail.com>
2018-01-08 10:59:11 -0800

    Clean code, add -, X, /

--- js/scripts.js
+++ js/scripts.js
@@ -28,78 +28,21 @@ var getInput = function (message){
 }
 
 // MAIN
-var number1 = parseInt(getInput("What is your first number?"));
-var number2 = parseInt(getInput("What is your second number?"));
+var number1 = parseInt(getInput("What is your first number to add?"));
+var number2 = parseInt(getInput("What is your second number to add?"));
 var result = add(number1, number2);
 alert("Your result is: \n"+ result);
 
-/*var add = function(number1, number2) {
-  return number1 + number2;
-};
+var number1 = parseInt(getInput("What is your first number to subtract?"));
+var number2 = parseInt(getInput("What is your second number to subtract?"));
+var result = subtract(number1, number2);
+alert("Your result is: \n"+ result);
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var number1 = parseInt(getInput("What is your first number to multiply?"));
+var number2 = parseInt(getInput("What is your second number to multiply?"));
+var result = multiply(number1, number2);
+alert("Your result is: \n"+ result);
 
-/*
-var greet = function (name) {
-	alert("Hello " + name + "!");
-}
-
-var subtractNumbers = function(){
-	firstNum = getInput("What is your first number");
-  secondNum = getInput("What is your second number");
-  newNum = subtract(parseInt(firstNum), parseInt(secondNum));
-  alert("Your result is: "+ newNum);
-}
-
-var multiplyNumbers = function(){
-	firstNum = getInput("What is your first number");
-  secondNum = getInput("What is your second number");
-  newNum = multiply(parseInt(firstNum), parseInt(secondNum));
-  alert("Your result is: "+ newNum);
-}
-
-var threeTimes = function(){
-	firstNum = getInput("What is your first number");
-  secondNum = getInput("What is your second number");
-  thirdNum = getInput("What is your third number");
-  newNum = multiply(parseInt(firstNum), parseInt(secondNum));
-  newNewNum = multiply(newNum, parseInt(thirdNum));
-  alert("Your result is: "+ newNewNum);
-}
-
-var divideNumbers = function(){
-	firstNum = getInput("What is your first number");
-  secondNum = getInput("What is your second number");
-  newNum = divide(parseInt(firstNum), parseInt(secondNum));
-  alert("Your result is: "+ newNum);
-}
-
-var remainder = function(){
-	firstNum = getInput("What is your first number");
-  secondNum = getInput("What is your second number");
-  newNum = modulo(parseInt(firstNum), parseInt(secondNum));
-  alert("Your result is: "+ newNum);
-}
-
-var combine= function(string1, string2){
-	msg="Your name is: " + string1;
-  msg=msg + "\nYour favorite food is: " + string2;
-	return(msg);
-}
-
-// MAIN
-/*greet(getInput("What is your name?"));
-subtractNumbers();
-multiplyNumbers();
-threeTimes();
-divideNumbers();
-remainder();
-
-
-var newName = getInput("Please enter your name:");
-var favFood = getInput("Please enter your favorite food:");
-alert(combine(newName, favFood));
-*/
+var number1 = parseInt(getInput("What is your first number to divide?"));
+var number2 = parseInt(getInput("What is your second number to divide?"));
+alert("Your result is: \n"+ divide(number1, number2));
