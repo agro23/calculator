384712ee20d9e9c63d30a4496c83aca69d742b4e
Andy Grossberg <andy.grossberg@gmail.com>
2018-01-08 10:37:45 -0800

    Add README.md and index.html

---
+++ README.md
@@ -0,0 +1,45 @@
+# _styling-with-classes_
+
+#### _A sample set of html files to learn classes for Epicodus, at home 01/03/18_
+
+#### By _Andy Grossberg_
+
+## Description
+
+_A starter set of webpages written at home for Epicodus._
+_This is a series of webpages to work with CSS concepts._
+_They are intended to practice CSS features such as classes._
+_Included so far are **paragraphs.html**, **fido.html**, **cupcakeheaven.html** and the **boring-lecture project**_
+_as well as stylesheets for all of them and their other respective pages._
+
+_NOTE: cupcakeheaven.html has images, text and links._
+_Also: Three elements (strong, a and h2) are of class 'flashy'_
+_and there is a class called best to highlight a featured flavor._
+
+_NOTE: boring-lecture has its own subdirectories included for CSS and IMG._
+
+## Setup/Installation Requirements
+
+* _Download the repository and connect to them via your browser._ *
+
+_This project will have a little bit of CSS but nothing every browser won't render._
+
+## Known Bugs
+
+_Presently there are no bugs_
+
+## Support and contact details
+
+_If you have any suggestions or comments about this site, please let us know_
+_Feel free to reach out to me at andy.grossberg@gmail.com_
+
+## Technologies Used
+
+_HTML_
+_CSS_
+
+### License
+
+*Free to use under the GNU General public license (GPL).*
+
+Copyright (c) 2018 **_Andy Grossberg, adapted from the Epicodus templates._**
---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+
+var saySomething = function (whatToSay){
+	alert(whatToSay);
+}
+
+var add = function(number1, number2){
+	return number1+number2;
+}
+
+var subtract = function(number1, number2){
+	return number1-number2;
+}
+
+var multiply = function(number1, number2){
+	return number1*number2;
+}
+
+var divide = function(number1, number2){
+	return number1/number2;
+}
+
+var modulo = function(number1, number2){
+	return number1%number2;
+}
+
+var getInput = function (message){
+	var value = prompt(message);
+	return value;
+}
+
+// MAIN
+var number1 = parseInt(getInput("What is your first number?"));
+var number2 = parseInt(getInput("What is your second number?"));
+alert("Your result is: \n" + add(number1, number2));
+
+/*
+var greet = function (name) {
+	alert("Hello " + name + "!");
+}
+
+var subtractNumbers = function(){
+	firstNum = getInput("What is your first number");
+  secondNum = getInput("What is your second number");
+  newNum = subtract(parseInt(firstNum), parseInt(secondNum));
+  alert("Your result is: "+ newNum);
+}
+
+var multiplyNumbers = function(){
+	firstNum = getInput("What is your first number");
+  secondNum = getInput("What is your second number");
+  newNum = multiply(parseInt(firstNum), parseInt(secondNum));
+  alert("Your result is: "+ newNum);
+}
+
+var threeTimes = function(){
+	firstNum = getInput("What is your first number");
+  secondNum = getInput("What is your second number");
+  thirdNum = getInput("What is your third number");
+  newNum = multiply(parseInt(firstNum), parseInt(secondNum));
+  newNewNum = multiply(newNum, parseInt(thirdNum));
+  alert("Your result is: "+ newNewNum);
+}
+
+var divideNumbers = function(){
+	firstNum = getInput("What is your first number");
+  secondNum = getInput("What is your second number");
+  newNum = divide(parseInt(firstNum), parseInt(secondNum));
+  alert("Your result is: "+ newNum);
+}
+
+var remainder = function(){
+	firstNum = getInput("What is your first number");
+  secondNum = getInput("What is your second number");
+  newNum = modulo(parseInt(firstNum), parseInt(secondNum));
+  alert("Your result is: "+ newNum);
+}
+
+var combine= function(string1, string2){
+	msg="Your name is: " + string1;
+  msg=msg + "\nYour favorite food is: " + string2;
+	return(msg);
+}
+
+// MAIN
+/*greet(getInput("What is your name?"));
+subtractNumbers();
+multiplyNumbers();
+threeTimes();
+divideNumbers();
+remainder();
+
+
+var newName = getInput("Please enter your name:");
+var favFood = getInput("Please enter your favorite food:");
+alert(combine(newName, favFood));
+*/
